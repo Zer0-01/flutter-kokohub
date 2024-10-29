ef82e483e8ebc1a4073b6e102b50816e99251c6f
Zer0-01 <anas.zulkifli2001@gmail.com>
2024-10-29 23:06:17 +0800

    fix: fix where when keyboard appear, render overflow error occur

--- lib/ui/signup/pages/signup_layout.dart
+++ lib/ui/signup/pages/signup_layout.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_kokohub/constants/app_colors.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
@@ -14,51 +15,81 @@ class SignupLayout extends StatelessWidget {
     final localizations = AppLocalizations.of(context)!;
     return SafeArea(
       child: Padding(
-        padding: const EdgeInsets.all(40.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(
-              AppLocalizations.of(context)!.signup_get_started,
-              style: const TextStyle(
-                fontFamily: 'Fredoka',
-                fontWeight: FontWeight.bold,
-                fontSize: 36,
-              ),
+        padding: const EdgeInsets.symmetric(horizontal: 40.0),
+        child: SingleChildScrollView(
+          child: ConstrainedBox(
+            constraints:
+                BoxConstraints(minHeight: MediaQuery.of(context).size.height),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 20.0),
+                  child: Text(
+                    AppLocalizations.of(context)!.signup_get_started,
+                    style: const TextStyle(
+                      fontFamily: 'Fredoka',
+                      fontWeight: FontWeight.bold,
+                      fontSize: 36,
+                    ),
+                  ),
+                ),
+                SignUpDropdownWidget(
+                  text: AppLocalizations.of(context)!.signup_role,
+                  dropdownMenuEntries:
+                      roles.map<DropdownMenuEntry<String>>((String value) {
+                    return DropdownMenuEntry(value: value, label: value);
+                  }).toList(),
+                ),
+                SignUpTextFieldWidget(
+                  text: localizations.signup_first_name,
+                ),
+                SignUpTextFieldWidget(
+                  text: localizations.signup_last_name,
+                ),
+                SignUpTextFieldWidget(
+                  text: localizations.signup_ic_number,
+                ),
+                SignUpTextFieldWidget(
+                  text: localizations.signup_phone_number,
+                ),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: AppColors.loginColor2),
+                      child: const Text("Next")),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 20.0),
+                  child: RichText(
+                    textAlign: TextAlign.center,
+                    text: TextSpan(
+                      text: "Already have an account? ",
+                      style: const TextStyle(
+                        color: Colors.black, // Your main text color
+                      ),
+                      children: [
+                        TextSpan(
+                          text: "Log in",
+                          style: const TextStyle(
+                            color: Colors.blue, // Make it look like a link
+                            fontWeight: FontWeight.bold,
+                          ),
+                          recognizer: TapGestureRecognizer()
+                            ..onTap = () {
+                              Navigator.pop(context);
+                            },
+                        ),
+                      ],
+                    ),
+                  ),
+                )
+              ],
             ),
-            SignUpDropdownWidget(
-              text: AppLocalizations.of(context)!.signup_role,
-              dropdownMenuEntries:
-                  roles.map<DropdownMenuEntry<String>>((String value) {
-                return DropdownMenuEntry(value: value, label: value);
-              }).toList(),
-            ),
-            SignUpTextFieldWidget(
-              text: localizations.signup_first_name,
-            ),
-            SignUpTextFieldWidget(
-              text: localizations.signup_last_name,
-            ),
-            SignUpTextFieldWidget(
-              text: localizations.signup_ic_number,
-            ),
-            SignUpTextFieldWidget(
-              text: localizations.signup_phone_number,
-            ),
-            Align(
-              alignment: Alignment.centerRight,
-              child: ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                      backgroundColor: AppColors.loginColor2),
-                  child: const Text("Next")),
-            ),
-            const Text(
-              "Already have an account? Log in",
-              textAlign: TextAlign.center,
-            )
-          ],
+          ),
         ),
       ),
     );
